cda870442a7aa58734de68a2d596a1f4c48d75ae
Taissa Rodrigues <141452801+taissarodrigues@users.noreply.github.com>
2024-04-24 11:39:24 -0300

    test force push

--- bem-facil/bem-facil/ContentView.swift
+++ bem-facil/bem-facil/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "globeTeste")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
