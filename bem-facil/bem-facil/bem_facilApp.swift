7879c454437a7594b0f67789059ac08634736e5c
Taissa Rodrigues <141452801+taissarodrigues@users.noreply.github.com>
2024-04-24 11:24:53 -0300

    setup project

---
+++ bem-facil/bem-facil/bem_facilApp.swift
@@ -0,0 +1,17 @@
+//
+//  bem_facilApp.swift
+//  bem-facil
+//
+//  Created by User on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct bem_facilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
