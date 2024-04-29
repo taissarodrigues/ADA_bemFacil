5e4915102a1ab5efacc2e9fc24bd0e16d615f4d8
Taissa Rodrigues <141452801+taissarodrigues@users.noreply.github.com>
2024-04-29 11:55:33 -0300

    tela

--- bem-facil/bem-facil/App/Telainicio.swift
+++ bem-facil/bem-facil/App/Telainicio.swift
@@ -8,20 +8,71 @@
 import SwiftUI
 
 struct Telainicio: View {
-    var body: some View {
-        VStack {
-            Image("logo")
-               
-            
     
+    @State private var searchText: String = ""
+    
+    var body: some View {
+        VStack (alignment: .center) { 
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 50,height: 50)
+            
+            SearchBar(searchText: searchText)
+            
         }
-        .frame(maxWidth: .infinity,maxHeight: .infinity)
-       .background(Color("azulPrincipal"))/
-        .ignoresSafeArea()
         
-       
+        
+        
+//        ScrollView {
+            Spacer()
+            HStack (spacing:20) {
+                ForEach(0..<1){ index in
+                    Text("")
+                        .foregroundStyle(.white)
+                        .font(.largeTitle)
+                        .frame(width: 345, height: 150)
+                        .background(.black)
+                        .cornerRadius(8.0)
+//                        .padding(100)
+                                }
+                }
+
+//        }
+        
+        ScrollView {
+            HStack {
+                VStack (spacing:20) {
+                    ForEach(0..<5){ index in
+                        Text("")
+                            .foregroundStyle(.white)
+                            .font(.largeTitle)
+                            .frame(width: 150, height: 150)
+                            .background(.red)
+                            .cornerRadius(5.0)
+                        
+                                    }
+                }
+                VStack (spacing:20) {
+                    ForEach(0..<5){ index in
+                        Text("")
+                            .foregroundStyle(.white)
+                            .font(.largeTitle)
+                            .frame(width: 150, height: 150)
+                            .background(.red)
+                            .cornerRadius(5.0)
+                    }
+                }
+                
+            }
+//            .padding(70)
+
+            }
+        
+        }
+
     }
-}
+
 
 #Preview {
     Telainicio()
--- bem-facil/bem-facil/Features/SearchBar.swift
+++ bem-facil/bem-facil/Features/SearchBar.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct SearchBar: View {
+    @State var searchText: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack {
+            Image(systemName:"magnifyingglass")
+                .foregroundColor(.secondary)
+            
+            TextField("Pesquise aqui...", text: $searchText)
+                .foregroundColor(.black)
+                .overlay(
+                    Image(systemName: "mic")
+                        .padding()
+                        .offset(x:10)
+                        .foregroundColor(.black)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                    ,alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                .fill(Color.white)
+                .shadow(
+                    color: Color.black.opacity(0.15),
+                    radius: 10, x:0, y:0)
+        )
+        .padding()
     }
 }
 
-#Preview {
-    SearchBar()
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar()
+    }
 }
