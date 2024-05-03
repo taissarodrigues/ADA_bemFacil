5b3ef175bed5d64a7140d6ccb12cb9a4fad05a36
Taissa Rodrigues <141452801+taissarodrigues@users.noreply.github.com>
2024-05-03 11:42:23 -0300

    Adcionamos uma Tab para refazer a FAQ.

---
+++ bem-facil/bem-facil/Features/FaqTela.swift
@@ -0,0 +1,18 @@
+//
+//  FaqTela.swift
+//  bem-facil
+//
+//  Created by User on 03/05/24.
+//
+
+import SwiftUI
+
+struct FaqTela: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FaqTela()
+}
---
+++ bem-facil/bem-facil/Utils/TabTela.swift
@@ -0,0 +1,18 @@
+//
+//  TabTela.swift
+//  bem-facil
+//
+//  Created by User on 03/05/24.
+//
+
+import SwiftUI
+
+struct TabTela: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabTela()
+}
