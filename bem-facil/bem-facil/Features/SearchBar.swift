a929cb1cececa8743004c1c425e3fb327f724d40
Taissa Rodrigues <141452801+taissarodrigues@users.noreply.github.com>
2024-05-06 09:20:04 -0300

    muitas mudancas

---
+++ bem-facil/bem-facil/Features/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  bem-facil
+//
+//  Created by User on 06/05/24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
