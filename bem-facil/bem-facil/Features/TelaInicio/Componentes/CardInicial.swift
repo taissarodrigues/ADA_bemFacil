c9e95dd8b8385f8f2402f0feef08d735b8ad6451
K-mylly <164367073+K-mylly@users.noreply.github.com>
2024-05-07 10:48:32 -0300

    criei arquivo da view de card inicial

---
+++ bem-facil/bem-facil/Features/TelaInicio/Componentes/CardInicial.swift
@@ -0,0 +1,18 @@
+//
+//  CardInicial.swift
+//  bem-facil
+//
+//  Created by Kamylly Ferreira da Paixão on 07/05/24.
+//
+
+import SwiftUI
+
+struct CardInicial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardInicial()
+}
