f02e645185ef4a01c3ec1b68f5f2dd041bfc9497
Taissa Rodrigues <141452801+taissarodrigues@users.noreply.github.com>
2024-05-07 17:05:02 -0300

    Alteração pastas

---
+++ bem-facil/bem-facil/Features/TelaChat/Componentes/View/Models/ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModel.swift
+//  bem-facil
+//
+//  Created by Taissa Vitoria Rodrigues de Paula on 07/05/24.
+//
+
+import SwiftUI
+
+struct ViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModel()
+}
