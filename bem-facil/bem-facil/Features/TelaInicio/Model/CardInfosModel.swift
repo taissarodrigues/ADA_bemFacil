1cc3a159ee61825e40ded49c10c23e4f5a9c3a7c
cauanlgaldino <cauanlgaldino@gmail.com>
2024-05-13 11:59:13 -0300

    filter update
    update filtro

--- bem-facil/bem-facil/Features/TelaInicio/Model/CardInfosModel.swift
+++ bem-facil/bem-facil/Features/TelaInicio/Model/CardInfosModel.swift
@@ -14,13 +14,13 @@ struct CardInfoModel: Identifiable {
     var image: Image
     
     static let date: [CardInfoModel] = [
-        CardInfoModel(title: "Pé de meia", subTitle: "Educação", image: Image(.pedemeia)),
+        CardInfoModel(title: "Benefício Prestação Continuada", subTitle: "Assistência Social", image: Image(.bpc)),
         CardInfoModel(title: "Bolsa Familia", subTitle: "Assistência Social", image: Image(.bolsafamilia)),
         CardInfoModel(title: "Minha casa, Minha vida", subTitle: "Assistência Social", image: Image(.mcmv)),
-        CardInfoModel(title: "Desenrola Fies", subTitle: "Educação", image: Image(.desenrolaFies)),
         CardInfoModel(title: "Vale Gás", subTitle: "Assistência Social", image: Image(.valegas)),
-        CardInfoModel(title: "Benefício Prestação Continuada", subTitle: "Assistência Social", image: Image(.bpc)),
         CardInfoModel(title: "Bolsa Atleta", subTitle: "Cultura", image: Image(.bolsaAtleta)),
+        CardInfoModel(title: "Pé de meia", subTitle: "Educação", image: Image(.pedemeia)),
+        CardInfoModel(title: "Desenrola Fies", subTitle: "Educação", image: Image(.desenrolaFies)),
         CardInfoModel(title: "Farmácia Popular", subTitle: "Saúde", image: Image(.farmacia))
     ]
 }
--- bem-facil/bem-facil/Features/TelaInicio/View/Telainicio.swift
+++ bem-facil/bem-facil/Features/TelaInicio/View/Telainicio.swift
@@ -1,55 +1,80 @@
-//
-//  ContentView.swift
-//  bem-facil
-//
-//  Created by User on 24/04/24.
-//
-
 import SwiftUI
 
 struct Telainicio: View {
-    
     @State private var searchText: String = ""
-    
+    @State private var isShowingFilter: Bool = false // Variável para mostrar/ocultar o Picker
+    @State private var selectedCategory: String = "Todos" // Inicia com "Todos"
+
     let adaptiveColumns = Array(repeating: GridItem(.fixed(170)), count: 2)
+    
+    var filteredData: [CardInfoModel] {
+        if selectedCategory == "Todos" {
+            return CardInfoModel.date
+        } else {
+            return CardInfoModel.date.filter { $0.subTitle == selectedCategory }
+        }
+    }
     
     var body: some View {
         NavigationView {
-        
-        
-        VStack {
-            SearchBar(searchText: searchText)
-            
-            ScrollView(.horizontal) {
-                HStack(spacing:18) {
-                    ForEach(0..<3) { _ in
-                        Text("")
-                            .foregroundStyle(.white)
-                            .font(.largeTitle)
-                            .frame(width: 343, height: 180)
-                            .background(.black)
-                            .cornerRadius(8.0)
-                    }
-                }
-            }
-            .scrollIndicators(.hidden)
-            
-           
-                            ScrollView {
-                                LazyVGrid(columns: adaptiveColumns, spacing: 10) {
-                                    ForEach(CardInfoModel.date) { item in
-                                        NavigationLink(destination: TelaInfo()) {
-                                            CustomCard(model: item)
-                                        }
-                                    }
-                                }
-                                .navigationTitle(Text(""))
-                            }
+            VStack {
+                
+                
+                
+                SearchBar(searchText: searchText)
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing:18) {
+                        ForEach(0..<3) { _ in
+                            Text("")
+                                .foregroundStyle(.white)
+                                .font(.largeTitle)
+                                .frame(width: 343, height: 180)
+                                .background(.black)
+                                .cornerRadius(8.0)
                         }
                     }
                 }
+                .scrollIndicators(.hidden)
+                
+                HStack {
+                    Text("Programas")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading)
+                    
+                    Picker(selection: $selectedCategory, label: Text("Categoria")) {
+                        ForEach(["Todos", "Assistência Social", "Cultura", "Educação", "Saúde"], id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(DefaultPickerStyle())
+                    .padding()
+                    
+                        Image(systemName: "line.horizontal.3.decrease.circle")
+                            .foregroundColor(.blue)
+                            .padding(.trailing)
+                    }
+                
+                
+                ScrollView {
+                    LazyVGrid(columns: adaptiveColumns, spacing: 10) {
+                        ForEach(filteredData) { item in
+                            NavigationLink(destination: TelaInfo()) {
+                                CustomCard(model: item)
+                            }
+                        }
+                    }
+                    .navigationTitle(Text(""))
+                }
             }
+        }
+    }
+}
 
-#Preview {
-    Telainicio()
+struct Telainicio_Previews: PreviewProvider {
+    static var previews: some View {
+        Telainicio()
+    }
 }
