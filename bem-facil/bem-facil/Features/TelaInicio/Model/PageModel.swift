696c8794afd8ceb4b6aef3301ef6b0d03bf8cef2
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-05-15 20:35:10 -0300

    Fix lint errors at PageModel

--- bem-facil/bem-facil/Features/TelaInicio/Model/PageModel.swift
+++ bem-facil/bem-facil/Features/TelaInicio/Model/PageModel.swift
@@ -16,13 +16,8 @@ struct Page: Identifiable, Equatable {
     static var samplePage = Page(name: "Title Exemple", description: "The is exemple description for the app", imageUrl: "info1", tag: 0)
     
     static var samplePages: [Page] = [
-        Page(name: "Seu canal de informação", description: "bla bla bla texto cruel aqui bla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .",
-             imageUrl: "info1", tag: 0),
-        Page(name: "Sem burocracia", description: "bla bla bla texto cruel aqui bla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .",
-             imageUrl:
-                "info2", tag: 1),
-        Page(name: "Tudo isso em um só local", description: "bla bla bla texto cruel aqui bla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .",
-             imageUrl: "info2", tag: 2)
+        Page(name: "Seu canal de informação", description: "bla bla bla texto cruel aqui bla bla bla texto cruelaquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .", imageUrl: "info1", tag: 0),
+        Page(name: "Sem burocracia", description: "bla bla bla texto cruel aqui bla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .", imageUrl: "info2", tag: 1),
+        Page(name: "Tudo isso em um só local", description: "bla bla bla texto cruel aqui bla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .", imageUrl: "info2", tag: 2)
     ]
 }
-
