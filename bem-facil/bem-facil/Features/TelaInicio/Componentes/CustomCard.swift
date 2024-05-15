e5407a446aea0a5f2a4be234b1e43ca120101447
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-05-15 09:34:48 -0300

    Fix swiftLint

--- bem-facil/bem-facil/Features/TelaInicio/Componentes/CustomCard.swift
+++ bem-facil/bem-facil/Features/TelaInicio/Componentes/CustomCard.swift
@@ -12,45 +12,46 @@ struct CustomCard: View {
     var model: ProgramsModel
     
     var body: some View {
-           ZStack {
-               RoundedRectangle(cornerRadius: 20)
-                   .foregroundColor(Color.benUi(.azulClaro))
-                   .frame(width: 163, height: 163)
-               Image(model.image)
-               
-               LinearGradient(gradient: Gradient(colors: [(
-                   Color.benUi(.azul3)),
-                   Color.benUi(.azul)
-               ]),
-               startPoint: .center, endPoint: .bottom)
-               .opacity(0.3)
-               .frame(width: 163,height: 163)
-               .mask(RoundedRectangle(cornerRadius: 20))
-               
-               VStack(alignment: .leading, spacing: -25) {
-                   Text(model.title)
-                       .font(.system(size: 17, weight: .bold))
-                       .foregroundStyle(.white)
-                       .shadow(radius: 5)
-                       .padding()
-
-                   Text(model.category)
-                       .padding(5)
-                       .font(.system(size: 12))
-                       .foregroundColor(Color.benUi(.azul))
-                       .background(
-                           Rectangle()
-                                .cornerRadius(20)
-                               .foregroundColor(Color.benUi(.azulClaro))
-                       ).padding()
-
-               }.frame(maxWidth: 163, alignment: .leading)
-                   .frame(maxHeight: 163, alignment: .bottom)
-           
-           }
-           
-       }
-   }
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(Color.benUi(.azulClaro))
+                .frame(width: 163, height: 163)
+            Image(model.image)
+            
+            LinearGradient(gradient: Gradient(colors: [(
+                Color.benUi(.azul3)),
+                Color.benUi(.azul)
+            ]), startPoint: .center, endPoint: .bottom)
+            .opacity(0.3)
+            .frame(width: 163, height: 163)
+            .mask(RoundedRectangle(cornerRadius: 20))
+            
+            VStack(alignment: .leading, spacing: -25) {
+                Text(model.title)
+                    .font(.system(size: 17, weight: .bold))
+                    .foregroundStyle(.white)
+                    .shadow(radius: 5)
+                    .padding()
+                
+                Text(model.category)
+                    .padding(5)
+                    .font(.system(size: 12))
+                    .foregroundColor(Color.benUi(.azul))
+                    .background(
+                        Rectangle()
+                            .cornerRadius(20)
+                            .foregroundColor(Color.benUi(.azulClaro))
+                    )
+                    .padding()
+                
+            }
+            .frame(maxWidth: 163, alignment: .leading)
+            .frame(maxHeight: 163, alignment: .bottom)
+            
+        }
+        
+    }
+}
 
 #Preview {
     CustomCard(model: ProgramsModel(title: "Pé de Meia", category: "Educação", image: "pedemeia"))
--- bem-facil/bem-facil/Features/TelaInicio/Componentes/SearchBar.swift
+++ bem-facil/bem-facil/Features/TelaInicio/Componentes/SearchBar.swift
@@ -12,7 +12,7 @@ struct SearchBar: View {
     var body: some View {
         
         HStack {
-            Image(systemName:"magnifyingglass")
+            Image(systemName: "magnifyingglass")
                 .foregroundColor(.secondary)
             
             TextField("Pesquise aqui...", text: $searchText)
@@ -20,7 +20,7 @@ struct SearchBar: View {
                 .overlay(
                     Image(systemName: "mic")
                         .padding()
-                        .offset(x:10)
+                        .offset(x: 10)
                         .foregroundColor(.black)
                         .opacity(searchText.isEmpty ? 0.0 : 1.0)
                     ,alignment: .trailing
@@ -29,12 +29,9 @@ struct SearchBar: View {
         .font(.headline)
         .padding()
         .background(
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            RoundedRectangle(cornerRadius: 25)
                 .fill(Color.white)
-                .shadow(
-                    color: Color.black.opacity(0.15),
-                    radius: 10, x:0, y:0)
-        )
+                .shadow(color: Color.black.opacity(0.15), radius: 10, x: 0, y: 0))
         .padding()
     }
 }
