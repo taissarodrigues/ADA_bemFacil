28367edc11114d1c1129179ce41dcb510712a2c2
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-05-15 20:33:54 -0300

    Fix lint and identation erros at ProgramsData

--- bem-facil/bem-facil/Features/TelaInicio/Model/ProgramsData.swift
+++ bem-facil/bem-facil/Features/TelaInicio/Model/ProgramsData.swift
@@ -61,117 +61,114 @@ class ProgramsData {
             ]
         ),
         Program(
-                   name: "Minha casa, Minha vida",
-                   image: "mcmv",
-                   category: "Assistência Social",
-                   requirements: [
-                       "Estar matriculada em escola pública e ter baixa renda, ou;",
-                       "Estar em situação de rua, ou;",
-                       "Estar inscrita no Cadastro único (CadÚnico)."
-                   ],
-                   documents: [
-                       "Documento de identificação oficial com foto e o número do CPF;",
-                       "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
-                   ],
-                   urls: [
-                       ProgramLink(icon: "mcmv", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
-                   ]
-               ),
-               Program(
-                   name: "Vale Gás",
-                   image: "valegas",
-                   category: "Assistência Social",
-                   requirements: [
-                       "Estar matriculada em escola pública e ter baixa renda, ou;",
-                       "Estar em situação de rua, ou;",
-                       "Estar inscrita no Cadastro único (CadÚnico)."
-                   ],
-                   documents: [
-                       "Documento de identificação oficial com foto e o número do CPF;",
-                       "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
-                   ],
-                   urls: [
-                       ProgramLink(icon: "valegas", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
-                   ]
-               ),
-               Program(
-                   name: "Bolsa Atleta",
-                   image: "bolsaAtleta",
-                   category: "Cultura",
-                   requirements: [
-                       "Estar matriculada em escola pública e ter baixa renda, ou;",
-                       "Estar em situação de rua, ou;",
-                       "Estar inscrita no Cadastro único (CadÚnico)."
-                   ],
-                   documents: [
-                       "Documento de identificação oficial com foto e o número do CPF;",
-                       "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
-                   ],
-                   urls: [
-                       ProgramLink(icon: "bolsaAtleta", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
-                   ]
-               ),
-               Program(
-                   name: "Desenrola Fies",
-                   image: "desenrolaFies",
-                   category: "Educação",
-                   requirements: [
-                       "Estar matriculada em escola pública e ter baixa renda, ou;",
-                       "Estar em situação de rua, ou;",
-                       "Estar inscrita no Cadastro único (CadÚnico)."
-                   ],
-                   documents: [
-                       "Documento de identificação oficial com foto e o número do CPF;",
-                       "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
-                   ],
-                   urls: [
-                       ProgramLink(icon: "desenrolaFies", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
-                   ]
-               ),
-               Program(
-                   name: "Farmácia Popular",
-                   image: "farmacia",
-                   category: "Saúde",
-                   requirements: [
-                       "Estar matriculada em escola pública e ter baixa renda, ou;",
-                       "Estar em situação de rua, ou;",
-                       "Estar inscrita no Cadastro único (CadÚnico)."
-                   ],
-                   documents: [
-                       "Documento de identificação oficial com foto e o número do CPF;",
-                       "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
-                   ],
-                   urls: [
-                       ProgramLink(icon: "farmacia", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
-                   ]
-               )
-           ]
-       }
-            
-            struct Program: Identifiable, Equatable, Hashable {
-                let id: UUID = UUID()
-                let name: String
-                let image: String
-                let category: String
-                let requirements: [String]
-                let documents: [String]
-                let urls: [ProgramLink]
-                
-                // Program(name: "Bolsa Familia") == Program(name: "Pe de meia") false!
-                // swiftlint:disable identifier_name
-                static func == (_ lhs: Program, _ rhs: Program) -> Bool {
-                    lhs.name == rhs.name
-                }
-                // swiftlint:enable identifier_name
-                
-                func hash(into hasher: inout Hasher) {
-                    hasher.combine(name)
-                }
-            }
-            
-            struct ProgramLink: Identifiable {
-                let id: UUID = UUID()
-                let icon: String
-                let shortName: String
-                let linkURL: String
-            }
+            name: "Minha casa, Minha vida",
+            image: "mcmv",
+            category: "Assistência Social",
+            requirements: [
+                "Estar matriculada em escola pública e ter baixa renda, ou;",
+                "Estar em situação de rua, ou;",
+                "Estar inscrita no Cadastro único (CadÚnico)."
+            ],
+            documents: [
+                "Documento de identificação oficial com foto e o número do CPF;",
+                "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
+            ],
+            urls: [
+                ProgramLink(icon: "mcmv", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
+            ]
+        ),
+        Program(
+            name: "Vale Gás",
+            image: "valegas",
+            category: "Assistência Social",
+            requirements: [
+                "Estar matriculada em escola pública e ter baixa renda, ou;",
+                "Estar em situação de rua, ou;",
+                "Estar inscrita no Cadastro único (CadÚnico)."
+            ],
+            documents: [
+                "Documento de identificação oficial com foto e o número do CPF;",
+                "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
+            ],
+            urls: [
+                ProgramLink(icon: "valegas", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
+            ]
+        ),
+        Program(
+            name: "Bolsa Atleta",
+            image: "bolsaAtleta",
+            category: "Cultura",
+            requirements: [
+                "Estar matriculada em escola pública e ter baixa renda, ou;",
+                "Estar em situação de rua, ou;",
+                "Estar inscrita no Cadastro único (CadÚnico)."
+            ],
+            documents: [
+                "Documento de identificação oficial com foto e o número do CPF;",
+                "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
+            ],
+            urls: [
+                ProgramLink(icon: "bolsaAtleta", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
+            ]
+        ),
+        Program(
+            name: "Desenrola Fies",
+            image: "desenrolaFies",
+            category: "Educação",
+            requirements: [
+                "Estar matriculada em escola pública e ter baixa renda, ou;",
+                "Estar em situação de rua, ou;",
+                "Estar inscrita no Cadastro único (CadÚnico)."
+            ],
+            documents: [
+                "Documento de identificação oficial com foto e o número do CPF;",
+                "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
+            ],
+            urls: [
+                ProgramLink(icon: "desenrolaFies", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
+            ]
+        ),
+        Program(
+            name: "Farmácia Popular",
+            image: "farmacia",
+            category: "Saúde",
+            requirements: [
+                "Estar matriculada em escola pública e ter baixa renda, ou;",
+                "Estar em situação de rua, ou;",
+                "Estar inscrita no Cadastro único (CadÚnico)."
+            ],
+            documents: [
+                "Documento de identificação oficial com foto e o número do CPF;",
+                "Autorização do programa dignidade menstrual, gerada pelo app: Meus SUS digital."
+            ],
+            urls: [
+                ProgramLink(icon: "farmacia", shortName: "Meu SUS digital", linkURL: "https://meususdigital.saude.gov.br/login")
+            ]
+        )
+    ]
+}
+
+struct Program: Identifiable, Equatable, Hashable {
+    let id: UUID = UUID()
+    let name: String
+    let image: String
+    let category: String
+    let requirements: [String]
+    let documents: [String]
+    let urls: [ProgramLink]
+    
+    static func == (_ lhs: Program, _ rhs: Program) -> Bool {
+        lhs.name == rhs.name
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+}
+
+struct ProgramLink: Identifiable {
+    let id: UUID = UUID()
+    let icon: String
+    let shortName: String
+    let linkURL: String
+}
