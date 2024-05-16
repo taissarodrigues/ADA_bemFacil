75a73361f750bd7500c524ebc879e690c820731d
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-05-15 23:07:53 -0300

    Custom text for each description at tab view

--- bem-facil/bem-facil/Features/OnBoard/Model/OnBoardModel.swift
+++ bem-facil/bem-facil/Features/OnBoard/Model/OnBoardModel.swift
@@ -13,9 +13,10 @@ struct OnBoardModel: Identifiable, Hashable {
     let image: String
     
     static let list: [OnBoardModel] = [
-        OnBoardModel(id: 0, title: "Seu canal de informação", description: "bla bla bla texto cruel aqui bla bla bla texto cruelaquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .", image: "info1"),
-        OnBoardModel(id: 1, title: "Sem burocracia", description: "bla bla bla texto cruel aqui bla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .", image: "info2"),
-        OnBoardModel(id: 2, title: "Tudo isso em um só local", description: "bla bla bla texto cruel aqui bla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .", image: "info3"),
-        OnBoardModel(id: 3, title: "Rápido e prático", description: "bla bla bla texto cruel aqui bla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla texto cruel aquibla bla bla .", image: "info4")
+        OnBoardModel(id: 0, title: "Seu canal de informação", description: "Encontre tudo o que precisa saber sobre nossos programas de governo aqui, com detalhes e atualizações frequentes para mantê-lo sempre informado.", image: "info1"),
+        OnBoardModel(id: 1, title: "Sem burocracia", description: "Facilitamos o acesso às informações sem complicações ou entraves, garantindo que você encontre o que precisa sem perder tempo.", image: "info2"),
+        OnBoardModel(id: 2, title: "Tudo isso em um só local", description: "Acesse todos os serviços e programas de governo em um só lugar, centralizando suas necessidades de forma conveniente e organizada.", image: "info3"),
+        OnBoardModel(id: 3, title: "Rápido e prático", description: "Encontre informações de forma rápida e fácil, sem perder tempo, com uma interface intuitiva que facilita sua navegação.", image: "info4")
+
     ]
 }
